187606ea3f6f6dee9cb67801e4294d006272fd20
Javdu10 <jav.valentin@gmail.com>
2020-03-04 22:54:58 +0100

    add new module Azlink

---
+++ Symbioz.World/Modules/Azlink.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Symbioz.Core;
+using Symbioz.Core.DesignPattern.StartupEngine;
+
+namespace Symbioz.World.Modules
+{
+    class Azlink
+    {
+        static Logger logger = new Logger();
+        [StartupInvoke("Azlink", StartupInvokePriority.Modules)]
+        public static void Initialize()
+        {
+            logger.Alert("Azlink Started! ");
+        }
+
+    }
+}
